f44ff40bd137c1b6ab4d3f521747bf70ea515861
JayneNjeri <jaynenjeri739@gmail.com>
2023-04-17 18:12:32 -0700

    4. SUM

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "4-sum.h"
+
+/**
+ * main - uses the sum function to sum 2 num
+ *
+ * Return: returns a 0 when correct.
+ */
+int main(void)
+{
+	int ans;
+
+	ans = SUM(8100, 23);
+	printf("%d\n", ans);
+	return (0);
+}
