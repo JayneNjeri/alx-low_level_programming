685ea9dd27b711e737b5ec1826a95eb5c6eb51f6
JayneNjeri <jaynenjeri739@gmail.com>
2023-04-17 17:38:17 -0700

    0. Object-like Macro

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - where the programs strats from
+ *
+ * Return: 0 when correct.
+ */
+int main(void)
+{
+	int jayne;
+
+	jayne = 98 + SIZE;
+	printf("%d\n", jayne);
+	return (0);
+}
+
